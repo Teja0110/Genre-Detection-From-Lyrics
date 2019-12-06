29a5ba09fd6094d892fb25f7aebffb1842a0ba6b
Bao Nguyen <baocon1320@gmail.com>
2019-12-06 12:25:08 -0800

    modify predict

--- predict.py
+++ predict.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 # For export model
 from sklearn.externals import joblib
@@ -24,8 +25,24 @@ def preprocess_a_lyric(input):
 
 
 def predict_a_song(input, clf):
+  #load_model_dir = 'Model/genrePredict.pkl'
+  #clf = joblib.load(load_model_dir) 
   out = preprocess_a_lyric(input)
   return clf.predict([out])[0]
   
 
+def main(argv):
+    if len(argv) != 2:
+        print('usage python3 predict.py input')
+        sys.exit()
 
+    f = open(argv[1], "r")
+    
+    data = f.read()
+    load_model_dir = 'Model/genrePredict.pkl'
+    clf = joblib.load(load_model_dir)
+    print("Genre of this lyric is ", predict_a_song(data, clf))
+
+
+if __name__ == '__main__':
+    main(sys.argv)
